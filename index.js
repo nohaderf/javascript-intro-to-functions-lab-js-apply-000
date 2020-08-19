d5f0b7db26c1f9b312988c7742d926fc6a49620d
Freda Hon <freda.hon@gmail.com>
2020-08-19 03:40:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string){
   return "I can't hear you!";
   } if (string.toUpperCase() === string){
     return "YES INDEED!";
-  } if (string === loveGrandma){
+  } if (string === "I love you, Grandma."){
     return "I love you, too.";
   }
 }
